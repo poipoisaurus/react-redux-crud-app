ccf7b733c4b9f890bb38c7121fec4ba90904ac7d
arase taisuke <arase@qnote.co.jp>
2020-01-25 19:39:16 +0900

    [add] イベント一覧を実装

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+export const READ_EVENTS = 'READ_EVENTS'
+
+const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
+const QUERY_STRING = '?token=token123'
+
+export const readEvents = () => async dispatch => {
+  const res = await axios.get(`${ROOT_URL}/events${QUERY_STRING}`)
+  dispatch({
+    type: READ_EVENTS,
+    res
+  })
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+import reducer from './reducers'
 import './index.css';
-import App from './App';
+import EventIndex from './components/Event';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <EventIndex />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
